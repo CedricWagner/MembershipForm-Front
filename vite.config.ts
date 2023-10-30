2a4d39ef44ff0e853dab575438be000d778ab420
Cédric Wagner <wagner.ced@gmail.com>
2023-10-31 00:29:40 +0100

    prepare pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import type { UserConfig as VitestUserConfigInterface } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 const vitestConfig: VitestUserConfigInterface = {
   test: {
@@ -13,7 +14,48 @@ const vitestConfig: VitestUserConfigInterface = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: [
+        "/logo-32x32.webp",
+        "/logo-192x192.png",
+        "/logo-180x180.webp",
+        "/favicon.ico",
+      ],
+      manifest: {
+        name: "Gestion des adhésions",
+        short_name: "AppAdhesion",
+        description:
+          "Application permettant la gestion des adhésions de la Maison Citoyenne",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   test: vitestConfig.test,
   resolve: {
     alias: {
