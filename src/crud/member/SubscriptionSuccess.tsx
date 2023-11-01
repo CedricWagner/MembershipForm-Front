2a1cd62ddae886c8455d91dfeb76f8724b5cfad5
Cédric Wagner <wagner.ced@gmail.com>
2023-11-01 14:06:04 +0100

    add links to subscription success page

--- src/crud/member/SubscriptionSuccess.tsx
+++ src/crud/member/SubscriptionSuccess.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import { useParams } from "react-router-dom";
 import Container from "../../components/Container/Container";
 import NavigationBlock from "../../components/NavigationBlock/NavigationBlock";
 import SuccessMessage from "../../components/SuccessMessage/SuccessMessage";
-import { HiUserAdd } from "react-icons/hi";
+import { HiCalendar, HiHome, HiUserAdd } from "react-icons/hi";
+import { BiHomeSmile } from "react-icons/bi";
 
 interface SubscriptionSuccessProps {}
 
@@ -13,7 +14,7 @@ const SubscriptionSuccess: FC<SubscriptionSuccessProps> = () => {
   return (
     <div data-testid="SubscriptionSuccess">
       <Container>
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center text-center md:text-left">
           <SuccessMessage>
             Adhésion enregistrée ! <br />
             Votre numéro d'adhérent est le{" "}
@@ -25,6 +26,24 @@ const SubscriptionSuccess: FC<SubscriptionSuccessProps> = () => {
             picto={<HiUserAdd size={40} />}
             title="Saisir une nouvelle adhésion"
           />
+          <a
+            href="https://www.maison-citoyenne.org/"
+            className="btn btn-primary"
+          >
+            <HiHome className="inline-block" size={20} />
+            &nbsp;Accéder au site de la Maison Citoyenne
+          </a>
+          <a
+            href="https://www.maison-citoyenne.org/agenda/"
+            className="btn btn-primary"
+          >
+            <HiCalendar className="inline-block" size={20} />
+            &nbsp;Voir nos événements
+          </a>
+          <p className="flex items-center pt-2 text-xl">
+            <span className="pr-2">Bienvenue parmis nous !</span>
+            <BiHomeSmile className="inline-block" size={40} />
+          </p>
         </div>
       </Container>
     </div>
